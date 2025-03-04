052e3979517f00826ff775f37d17137f93b9f7e1
Jgonzalez <jesusjoelgonzalezcastro@gmail.com>
2025-03-03 21:36:55 -0500

    Add HTTP error interceptor to app configuration
    Integrated the `errorsInterceptor` using `withInterceptors` in the HTTP client setup. This enhances global error handling across the application by centralizing HTTP error management.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
@@ -6,11 +6,12 @@ import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 import { MessageService } from 'primeng/api';
+import { errorsInterceptor } from './app/core/http/interceptors/errors.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
-        provideHttpClient(withFetch()),
+        provideHttpClient(withFetch(), withInterceptors([errorsInterceptor])),
         provideAnimationsAsync(),
         providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
         MessageService
